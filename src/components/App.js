e83af2c3cb9a8d1734ade883d002ced1dcc5c46e
Zach Callanan <caveofmonsters@gmail.com>
2020-09-30 16:00:59 +0200

    removed unused import from app

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import Form from './Form';
 import Column from './Column';
 import Flag from './Flag';
